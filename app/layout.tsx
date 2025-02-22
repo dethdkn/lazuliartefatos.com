df547c5fc24b4216c818119616c0f4049e46d479
Gabriel Rosa <gabriel@rosa.dev.br>
2025-02-22 18:30:53 -0300

    ✨ metadata setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
+// import Nav from '@/components/Nav'
 import { Analytics } from '@vercel/analytics/next'
+import { Roboto } from 'next/font/google'
 import './tailwind.css'
 
 export const metadata: Metadata = {
@@ -8,10 +10,13 @@ export const metadata: Metadata = {
   description: 'Lojinha de arte.fatos - ecobags feitas à mão sob encomenda e LP\'s dos clássicos aos mais raros. desvenda...',
 }
 
-export default function RootLayout({ children }: Readonly<{ children: ReactNode }>){
+const roboto = Roboto({ subsets: ['latin'], weight: '400' })
+
+export default function IndexLayout({ children }: Readonly<{ children: ReactNode }>){
   return (
     <html lang="pt-BR">
-      <body>
+      <body className={roboto.className}>
+        {/* <Nav /> */}
         {children}
         <Analytics />
       </body>
---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap{
+  return [
+    {
+      url: 'https://lazuliartefatos.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    // {
+    //   url: 'https://lazuliartefatos.com/ecobags',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.9,
+    // },
+    // {
+    //   url: 'https://lazuliartefatos.com/ceramica',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.9,
+    // },
+    // {
+    //   url: 'https://lazuliartefatos.com/quadros',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.9,
+    // },
+    // {
+    //   url: 'https://lazuliartefatos.com/lps',
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.9,
+    // },
+  ]
+}
